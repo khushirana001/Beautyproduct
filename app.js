ccfc0d18ff08a5aae893a598fa842195b7519eaa
khushi <kushirana639642@gmail.com>
2025-05-12 15:27:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// function addToCart(productName) {
+//     alert(`${productName} has been added to your cart 🛍️`);
+//   }
+  
+function addToCart(productName) {
+    alert(`${productName} has been added to your cart 🛍️`);
+  }
+  
+  
